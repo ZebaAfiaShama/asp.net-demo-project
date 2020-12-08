db9a1827c255c922d31061c946567ea942db5c09
ZebaAfiaShama <zebashama2@gmail.com>
2020-12-08 14:36:02 +0600

    Initial Commit

---
+++ Joydebpur Govt. Girls High School/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Joydebpur_Govt._Girls_High_School.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Faculty()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            return View();
+        }
+        public IActionResult Facility()
+        {
+            return View();
+        }
+        public IActionResult Events()
+        {
+            return View();
+        }
+        public IActionResult Academic()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Result()
+        {
+            return View();
+        }
+        }
+}
---
+++ Joydebpur Govt. Girls High School/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Joydebpur_Govt._Girls_High_School.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
